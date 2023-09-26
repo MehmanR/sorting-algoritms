a40b6a766070b771f5a2f29675a19695302827d6
User <rustamzademehman96@gmail.com>
2023-09-26 15:48:07 +0400

    bubblesort added

--- SortingAlgorithms/src/Main.java
+++ SortingAlgorithms/src/Main.java
@@ -1,13 +1,10 @@
-import file.FileService;
-import file.inter.FileServiceInter;
-import global.GlobalStrings;
-
-import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args)  {
 
 
-
+//        int[] testArray = {8,6,10,4,9};
+//        int[] testArray2 = {16,2,4,6,10,18,12,8,14,};
+//        int[] testArray3 = {14,12,10};
     }
 }
--- SortingAlgorithms/src/metods/impl/BubbleSort.java
+++ SortingAlgorithms/src/metods/impl/BubbleSort.java
@@ -1,11 +1,34 @@
 package metods.impl;
-
 import metods.SortAlgorithm;
+
 
 public class BubbleSort implements SortAlgorithm {
 
     @Override
     public void sort(int[] array) {
+
+        int numOfStep = 0;
+        for (int i = 0; i < array.length; i++) {
+            boolean isSorted = true;
+            for (int j = 1; j < array.length; j++) {
+
+                if (array[j - 1] > array[j]) {
+                    isSorted = false;
+                    int temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+            numOfStep += i;
+            if (isSorted) {
+                break;
+            }
+        }
+        System.out.println("Number of step : " + numOfStep);
+        for (int test : array) {
+            System.out.print(test + " ");
+        }
+
 
     }
 }
