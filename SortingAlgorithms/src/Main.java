e0e212786446c211846dadf9a05377075dc15089
User <rustamzademehman96@gmail.com>
2023-09-30 13:28:30 +0400

    mergesort added

--- SortingAlgorithms/src/Main.java
+++ SortingAlgorithms/src/Main.java
@@ -1,10 +1,19 @@
+import metods.SortAlgorithm;
+import metods.impl.MergeSort;
 
 public class Main {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
 
-//        int[] testArray = {8,6,10,4,9};
-//        int[] testArray2 = {16,2,4,6,10,18,12,8,14,};
-//        int[] testArray3 = {14,12,10};
+        int[] testArray = {8, 6, 10, 4, 9, 3};
+        int[] testArray2 = {16, 2, 4, 6, 10, 151,18, 12, 8, 14,-99, 99};
+        int[] testArray3 = {14, 12, 10};
+
+        SortAlgorithm sortAlgorithm = new MergeSort();
+        sortAlgorithm.sort(testArray2);
+
+        for (int arr : testArray2) {
+            System.out.print(arr + " ");
+        }
     }
 }
--- SortingAlgorithms/src/metods/impl/MergeSort.java
+++ SortingAlgorithms/src/metods/impl/MergeSort.java
@@ -5,6 +5,43 @@ import metods.SortAlgorithm;
 public class MergeSort implements SortAlgorithm {
     @Override
     public void sort(int[] array) {
+        //     int[] testArray = {8,6,10,4,9};
+        if (array.length <= 1) {
+            return;
+        }
 
+        int midIndex = array.length / 2;
+        int[] leftArray = new int[midIndex]; //length 2
+        int[] rightArray = new int[array.length - midIndex];  // length 5-2 = 3
+
+        for (int i = 0; i < leftArray.length; i++) {
+            leftArray[i] = array[i];
+        }
+        for (int i = midIndex; i < array.length; i++) { //mid - 2
+            rightArray[i - midIndex] = array[i];
+        }
+
+        sort(leftArray);
+        sort(rightArray);
+        merge(array, leftArray, rightArray);
+    }
+
+    private void merge(int[] array, int[] leftArray, int[] rightArray) {
+        int i = 0, j = 0, k = 0;
+
+        while (i < leftArray.length && j < rightArray.length) {
+            if (leftArray[i] > rightArray[j]) {
+                array[k] = rightArray[j++];
+            } else {
+                array[k] = leftArray[i++];
+            }
+            k++;
+        }
+        while (i < leftArray.length) {
+            array[k++] = leftArray[i++];
+        }
+        while (j < rightArray.length) {
+            array[k++] = rightArray[j++];
+        }
     }
 }
